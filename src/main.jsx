65e9901d3d75ee5eccd0a4518301fed82259d2f9
Nimet Eyayu <yusrakamran2025@gmail.com>
2026-06-25 23:48:19 +0300

    fix client side routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // <-- YOU NEED THIS IMPORT
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/my_islamic_todo_app">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
